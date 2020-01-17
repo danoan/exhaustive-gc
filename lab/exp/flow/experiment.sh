e5e9cf7b1b1aa897cc34eadf327614f0ee5ba9c3
danoan <danoan2008@gmail.com>
2020-01-17 12:15:20 +0100

    Template render in flow experiments

--- lab/exp/flow/experiment.sh
+++ lab/exp/flow/experiment.sh
@@ -8,8 +8,7 @@ SUMMARY_APP=$1
 DATA_OUTPUT_FOLDER=${SCRIPT_PATH}/output
 PLOT_OUTPUT_FOLDER=${SCRIPT_PATH}/output/plot
 
-python3 ${SCRIPT_PATH}/experiment.py $ROOT_FOLDER cmake-build-release
-${SCRIPT_PATH}/summary-flow.sh $SUMMARY_APP $DATA_OUTPUT_FOLDER
+python3 ${SCRIPT_PATH}/instance-generator/generator.py $ROOT_FOLDER cmake-build-release $SUMMARY_APP
 
 
 ${SCRIPT_PATH}/plot-scripts/shape-grid.sh $DATA_OUTPUT_FOLDER $PLOT_OUTPUT_FOLDER ii 5 0.01000
--- lab/exp/flow/summary-flow.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-
-SUMMARY_APP=$1
-DATA_FOLDER=$( realpath $2 )
-
-find $DATA_FOLDER -name "inputData.txt" | xargs -I{} realpath {} | xargs -I{} dirname {} | xargs -I{} $SUMMARY_APP {} {}/summary.eps -j 5
